8ece057a58acee00b2a77eee5b5bf85cdc45cd4c
Lei Chen <lchen22643@sina.com>
2021-11-30 13:54:00 -0800

    Update DBfirstVersion.java

--- lyftInterview/DBfirstVersion.java
+++ lyftInterview/DBfirstVersion.java
@@ -3,80 +3,97 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.*;
 import  java.util.*;
-import java.util.concurrent.DelayQueue;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    private static String[] formalInput(String s){
+        String[] output = s.split("//pP");
+        for(int i = 0; i < output.length; i++){
+            output[i] = output[i].trim();
+        }
+        if(output.length > 0) output[0] = output[0].toUpperCase();
+        return output;
+    }
+    public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("result.txt")));
-        int q = Integer.parseInt(bufferedReader.readLine().trim());
-        
-        List<List<String>> settings = new ArrayList<>();
-        List<List<Integer>> settingNums = new ArrayList<>();
+        int inputSize= Integer.parseInt(bufferedReader.readLine().trim());
 
-        for (int qItr = 0; qItr < q; qItr++) {
+        List<List<String>> commandLines = new ArrayList<>();
+        List<List<Integer>> commandOptions = new ArrayList<>();
+
+        for (int readCount = 0; readCount < inputSize; readCount++) {
+
             String s = bufferedReader.readLine();
-            String[] ways = s.split("\\pP");
+            String[] commandInputs = formalInput(s);
+            List<String> subCommand = new ArrayList<>();
+            boolean skipSecIndex = (commandInputs[0].equals("GET")
+                                || commandInputs[0].equals("SET")
+                                || commandInputs[0].equals("UNSET"));
 
-            List<String> partList = new ArrayList<>();
-            partList.add(ways[0]);
-
-            boolean skipSecIndex = ways[0].trim().equals("get") || ways[0].trim().equals("set") || ways[0].trim().equals("unSet");
+            subCommand.add(commandInputs[0]);
             if(skipSecIndex){
-                partList.add(ways[1]);
+                subCommand.add(commandInputs[1]);
             }
-            settings.add(partList);
-            List<Integer> subList = new ArrayList<>();
-            for(int j = skipSecIndex ? 2 : 1; j < ways.length; j++){
-                subList.add(Integer.valueOf(ways[j].trim()));
+            commandLines.add(subCommand);
+
+            List<Integer> subCommandOptions = new ArrayList<>();
+            for(int j = skipSecIndex ? 2 : 1; j < commandInputs.length; j++){
+                subCommandOptions.add(Integer.valueOf(commandInputs[j]));
             }
-            settingNums.add(subList);
-            //   bufferedWriter.write(String.valueOf(result));
-           // bufferedWriter.newLine();
+            commandOptions.add(subCommandOptions);
         }
 
-        int i = 0, value, timeStep;
+        int commandPtr = 0, value, timeStep;
         String key;
-        DataBase db = null;
-        Deque<List<String>> settingStack = new ArrayDeque<>();
-        Deque<List<Integer>> settingNumsStack = new ArrayDeque<>();
-        
-        while(i < settings.size()){
-            List<String> setting = settings.get(i);
-            List<Integer> settingNum = settingNums.get(i);
-            
-      /*  }
-        while(){
-            List<String> setting = settingStack.pollFirst();
-            List<Integer> settingNum = settingNumsStack.pollFirst();*/
-            switch(setting.get(0)){
-                case "DataBase" :
-                    if(db == null) db = new DataBase();
-                    break;
-                case "get":
-                    key = setting.get(1);
-                    timeStep = settingNum.get(0);
+        DataBase db = new DataBase();
+        Deque<List<String>> commandLineQueue = new ArrayDeque<>();
+        Deque<List<Integer>> commandOptionQueue = new ArrayDeque<>();
+
+        while(commandPtr < commandLines.size()){
+            List<String> commandLine = commandLines.get(commandPtr);
+            List<Integer> commandLineOption = commandOptions.get(commandPtr);
+            if(commandLine.get(0).equals("ROLLBACK")){
+                if(commandLineQueue.peekLast().isEmpty() || !commandLineQueue.peekLast().get(0).equals("COMMIT")){
+                    throw new Exception("it is an illegal rollback");
+                }
+                while(!commandLineQueue.peekLast().get(0).equals("BLOCK")){
+                    commandLineQueue.pollLast();
+                    commandOptionQueue.pollLast();
+                }
+            }else{
+                commandLineQueue.offer(commandLine);
+                commandOptionQueue.offer(commandLineOption);
+            }
+            commandPtr++;
+        }
+        while(!commandLineQueue.isEmpty()){
+            List<String> processCommand = commandLineQueue.pollFirst();
+            List<Integer> processCommandOption = commandOptionQueue.pollFirst();
+            switch(processCommand.get(0)){
+                case "GET":
+                    key = processCommand.get(1);
+                    timeStep = processCommandOption.get(0);
                     db.get(key, timeStep);
                     break;
-                case "set":
-                    key = setting.get(1);
-                    value = settingNum.get(0);
-                    timeStep = settingNum.get(1);
+                case "SET":
+                    key = processCommand.get(1);
+                    value = processCommandOption.get(0);
+                    timeStep = processCommandOption.get(1);
                     db.set(key, value, timeStep);
                     break;
-                case "numWithValue":
-                    value = settingNum.get(0);
-                    int byKey = settingNum.get(1);
+                case "NUMWITHVALUE":
+                    value = processCommandOption.get(0);
+                    int byKey = processCommandOption.get(1);
                     db.numWithValue(value, byKey);
                     break;
-                case "unSet":
-                    key = setting.get(1);
+                case "UNSET":
+                    key = processCommand.get(1);
                     db.unSet(key);
                     break;
-
+                default:
+                    continue;
             }
-            i++;
+
+
         }
         bufferedReader.close();
       //  bufferedWriter.close();
